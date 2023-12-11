da61f4950b817c45ad355b95d97b548aad485cfc
leandro <leandro.dev.arg@gmail.com>
2023-12-11 17:00:32 -0300

    subida inicial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace U3_programa3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             float compra,desc,cfinal;
+            Console.WriteLine("Que valor tiene tu compra?");
+            compra=float.Parse(Console.ReadLine());
+
+            if (compra>=5000){
+                desc=(18*compra)/100;
+                cfinal=compra-desc;
+                Console.WriteLine("tu compra tiene 18% de descuento, el valor es "+ cfinal);
+            }else if (compra>=1000 && compra<5000){
+                desc=(10*compra)/100;
+                cfinal=compra-desc;
+                Console.WriteLine("tu compra tiene 10% de descuento, el valor es "+ cfinal);
+            }else{
+                Console.WriteLine("tu compra  no tiene  descuento, el valor es "+ compra);
+            }
+        }
+    }
+}
